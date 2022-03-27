337ae8a0f4ea80fe7dd7b76e3ebc643a9bd1afba
Mykel-AG <adegboromichael@gmail.com>
2022-03-27 03:09:00 +0100

    Updated cors origin

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ const Routes = require('./app/routes');
 const db = require("./app/models");
 
 //Add origin
-
 app.use(cors({ origin: ["http://localhost:8081", "http://localhost:3000", "https://623fc08290a68f6e2e84b64b--keen-fox-5359b3.netlify.app"],  }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
